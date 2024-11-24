29f9e0ef7ff7e301efadad9db7ab0332eae3ef44
ChaseRensberger <chasehrensberger@gmail.com>
2024-11-24 18:35:30 -0500

    add storage

--- infra/storage.ts
+++ infra/storage.ts
@@ -1,1 +1,9 @@
 export const bucket = new sst.aws.Bucket("Uploads");
+
+export const table = new sst.aws.Dynamo("TrackedChannels", {
+  fields: {
+    userId: "string",
+    channelId: "string",
+  },
+  primaryIndex: { hashKey: "userId", rangeKey: "channelId" },
+});
--- packages/core/sst-env.d.ts
+++ packages/core/sst-env.d.ts
@@ -10,6 +10,10 @@ declare module "sst" {
       "type": "sst.aws.ApiGatewayV2"
       "url": string
     }
+    "TrackedChannels": {
+      "name": string
+      "type": "sst.aws.Dynamo"
+    }
     "Uploads": {
       "name": string
       "type": "sst.aws.Bucket"
