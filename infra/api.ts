99768188e5a11ba69af1f79a66a996adfc928077
ChaseRensberger <chasehrensberger@gmail.com>
2024-11-27 19:20:47 -0500

    secure api

--- infra/api.ts
+++ infra/api.ts
@@ -1,8 +1,31 @@
-import { bucket } from "./storage";
+// import { bucket } from "./storage";
+//
+// export const api = new sst.aws.ApiGatewayV2("Api");
+//
+// api.route("GET /", {
+//   link: [bucket],
+//   handler: "packages/functions/src/api.handler",
+// });
 
-export const api = new sst.aws.ApiGatewayV2("Api");
+import { table } from "./storage";
 
-api.route("GET /", {
-  link: [bucket],
-  handler: "packages/functions/src/api.handler",
+export const api = new sst.aws.ApiGatewayV2("Api", {
+  transform: {
+    route: {
+      handler: {
+        link: [table],
+      },
+      args: {
+        auth: { iam: true },
+      },
+    },
+  },
 });
+
+api.route("POST /tracked-channels", "packages/functions/src/create.main");
+api.route("GET /tracked-channels/{id}", "packages/functions/src/get.main");
+api.route("GET /tracked-channels", "packages/functions/src/list.main");
+api.route(
+  "DELETE /tracked-channels/{id}",
+  "packages/functions/src/delete.main",
+);
---
+++ packages/functions/src/create.ts
@@ -0,0 +1,31 @@
+import * as uuid from "uuid";
+import { Resource } from "sst";
+import { Util } from "@mutube-sst/core/util";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+
+const dynamoDb = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+
+export const main = Util.handler(async (event) => {
+  let data = {
+    content: "",
+    attachment: "",
+  };
+
+  if (event.body != null) {
+    data = JSON.parse(event.body);
+  }
+
+  const params = {
+    TableName: Resource.TrackedChannels.name,
+    Item: {
+      userId: "123", // The id of the author
+      channelId: uuid.v1(), // A unique uuid
+      createdAt: Date.now(), // Current Unix timestamp
+    },
+  };
+
+  await dynamoDb.send(new PutCommand(params));
+
+  return JSON.stringify(params.Item);
+});
